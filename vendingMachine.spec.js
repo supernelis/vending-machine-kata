be98dea4c7dde3b58bc7d1d4e7ad903c33808de4
Nelis Boucké <nelis@archiwise.com>
2020-04-18 20:15:08 +0200

    Display INSERT COIN on startup

--- vendingMachine.spec.js
+++ vendingMachine.spec.js
@@ -1,3 +1,16 @@
-it ("should pass", () => {
-    expect(false).toBe(true);
+
+// Matteo, Nelis, Saket
+
+describe ("accept coin", () => {
+   it ("displays INSERT COIN on startup", () => {
+       const display = jest.fn();
+       const vendingMachine = new VendingMachine(display);
+       expect(display).toHaveBeenCalledWith("INSERT COIN");
+   }); 
 });
+
+class VendingMachine{
+    constructor(display) {
+        display("INSERT COIN");
+    }
+}
